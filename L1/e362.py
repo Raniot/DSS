9fa327b2cb81e268fcb24c306ca78f678fafdb62
Anders Meidahl <Andersmeidahl@gmail.com>
2019-02-10 12:53:26 +0100

    Fixed and solved exercise 362 and 363

--- L1/e362.py
+++ L1/e362.py
@@ -3,8 +3,12 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm # import statsmodels 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 boston = load_boston()
+
+print(boston["DESCR"])
 
 bos = pd.DataFrame(boston["data"])
 bos.columns = boston["feature_names"]
@@ -12,9 +16,9 @@ bos['MDEV'] = boston["target"]
 print(bos.head())
 
 lm = LinearRegression()
-X = bos['LSTAT']
+XInit = bos['LSTAT']
 Y = bos['MDEV']
-X = sm.add_constant(X) ## let's add an intercept (beta_0) to our model
+X = sm.add_constant(XInit) ## let's add an intercept (beta_0) to our model
 
 model = sm.OLS(Y,X).fit()
 
@@ -24,3 +28,20 @@ model = sm.OLS(Y,X).fit()
 print(model.summary())
 print(X.shape)
 print(Y.shape)
+
+testData = [5, 10, 15]
+testData = sm.add_constant(testData)
+
+result = model.predict(testData)
+print(result)
+
+print(XInit.idxmax())
+
+plt.figure(figsize=(12,10))
+sns.regplot(XInit, Y,robust=True)
+plt.xlabel('% lower status of the population')
+plt.ylabel("Median value of owner-occupied homes in $1000's")
+plt.show()
+
+
+
---
+++ L1/e363.py
@@ -0,0 +1,39 @@
+from sklearn.datasets import load_boston
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm # import statsmodels 
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+boston = load_boston()
+
+print(boston["DESCR"])
+
+bos = pd.DataFrame(boston["data"])
+bos.columns = boston["feature_names"]
+bos['MDEV'] = boston["target"]
+
+print(bos.head())
+
+X = bos[['LSTAT', 'AGE']]
+Y = bos['MDEV']
+X = sm.add_constant(X) ## let's add an intercept (beta_0) to our model
+
+model = sm.OLS(Y,X).fit()
+print(model.summary())
+
+# Part 2
+
+X = bos[boston["feature_names"]]
+X = sm.add_constant(X) ## let's add an intercept (beta_0) to our model
+model = sm.OLS(Y,X).fit()
+print(model.summary())
+
+# Part 3
+vifResult = pd.Series([variance_inflation_factor(X.values, i) 
+               for i in range(X.shape[1])], 
+              index=X.columns)
+
+print(vifResult)
